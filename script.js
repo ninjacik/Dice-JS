9557784fc7340f99b5fb2521c985c395d180b47b
Nyoman Triandi <andraemail.com@gmail.com>
2026-05-12 22:44:33 +0800

    Enhance dice game win messages with flags
    Added victory flags to the win messages.

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function dice() {
     document.querySelector(".img1").setAttribute("src", randomImageSource2);
 
     if (randomNumber2 > randomNumber1) {
-        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
     } else if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
     } else {
-        document.querySelector("h1").innerHTML = "It's a Draw!";
+        document.querySelector("h1").innerHTML = "It's a Draw! ";
     }
 }
 document.querySelector("button").onclick = dice;
